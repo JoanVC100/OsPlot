9f86cd5675ec2251bd5fe18eee0a1708a1bb950a
JoanVC100 <jojovica2001@gmail.com>
2023-02-13 22:10:07 +0100

    Intent de trigger

--- bash/OsPlot.sh
+++ bash/OsPlot.sh
@@ -29,13 +29,34 @@ function bucle_lectura {
     stty -F $port raw speed 1000000 -echo > /dev/null # Necessari perque cat o read no es pengin
     n=$n_mostres
     s=""
+    captura=0 # 0 si no es pot capturar, 1 si sí es pot
+    nivell_trigger=1
+    n1_lectura=0 # Lectura anterior
+    en_pujada=1 # Flanc descendent sii és 0. Si hi ha flanc
+    #ascendent i la lectura esta per sota el trigger, és 1
     while read -r linia; do
-        printf -v s "%s%s\n" "$s" "$linia"
-        ((n=n-1))
-        if (( !n )); then
-            echo "$s" > $cua_dades
-            n=$n_mostres
-            s=""
+        n_lectura=$((10#$linia))
+        deriv=$(($n_lectura-$n1_lectura))
+        n1_lectura=$n_lectura
+        if (( $deriv > 0 && $n_lectura <= $nivell_trigger )); then
+            en_pujada=1
+        elif (( $deriv < 0 )); then
+            en_pujada=0
+        fi
+        if (( $captura == 1 )); then
+            printf -v s "%s%s\n" "$s" "$n_lectura"
+            ((n=n-1))
+            if (( !n )); then
+                echo "$s" > $cua_dades
+                n=$n_mostres
+                s=""
+                captura=0
+                en_pujada=0
+            fi
+        else
+            if (( $en_pujada == 1 && $n_lectura >= $nivell_trigger )); then
+                captura=1
+            fi
         fi
     done < $port
 }
